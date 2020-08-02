cd8046f16ed438e38c457038eac2ed8492afc94d
fouzan <Fouzan.khan468@gmail.com>
2020-08-02 21:17:02 +0530

    Add files via upload
    All of these files are various projects and teachings from udemy bootcamp course in python by jose portilla

---
+++ capitalizer.py
@@ -0,0 +1,4 @@
+'''Capitalizes the words its passed'''
+def cap_text(text):
+    '''Capitalizes all the first letters of the words in it'''
+    return text.title()
---
+++ check_pylint.py
@@ -0,0 +1,10 @@
+'''This program just checks for errors in the functions'''
+def func():
+    '''function for numbers'''
+    first = 3
+    second = 5
+    print(first)
+    print(second)
+    
+func()
+
